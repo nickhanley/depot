54374727e84aa0dc014bf2d536f0e172e5a586bb
nick.hanley <nick.hanley@gmail.com>
2014-07-03 09:36:13 -0700

    products listing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,50 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title:         'My Book Title',
+                          description:   'a description',
+                          image_url:     'sample.png')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['Must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 0
+    assert_equal ['Must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+
+  end
+
+  def new_product(image_url)
+    Product.new(title:         'My Book Title',
+                description:   'a description',
+                price:         1,
+                image_url:     image_url)
+  end
+
+  test 'image url' do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should not be valid"
+    end
+  end
 end
